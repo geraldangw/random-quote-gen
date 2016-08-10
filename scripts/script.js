255afc064e15d592dff07cd188fc98fd512b30a0
weiyang wan <weiyangwan@gmail.com>
2016-08-10 17:06:24 +0800

    Added API

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,30 @@
 $(function() {
 
-//var declarations for DOM elements
+  //var declarations for DOM elements
+  var $quoteOutPut = $('#quoteoutput');
+  var $submitbtn = $('#submitbtn');
+  var $loader = $('.loader');
 
-
-//click
-
-
-//ajax
-
-
-//results of .done, .fail and .always
-
-
+  //click
+  $submitbtn.on('click', function(e){
+    e.preventDefault();
+    //ajax
+    $.ajax({
+      url: "https://andruxnet-random-famous-quotes.p.mashape.com/",
+      datatype: 'json',
+      beforeSend: function(xhr) {
+        $loader.show();
+        xhr.setRequestHeader('X-Mashape-Key', 'YpxfYwYFvlmshzZFBd2DC10ufIwKp1SjBInjsnDFkfR1I32B7a');
+      },
+    }).done(function(data){
+      $loader.hide();
+      //front end rendering
+      console.log('API Success');
+    }).fail(function(request, textStatus, errorThrown){
+      $loader.hide();
+      console.log("API Fail");
+      //front end rendering
+    });
+  });
 
 });
