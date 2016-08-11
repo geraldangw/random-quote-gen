b6d9219396d38a2a7839d432aa2012f00b157364
weiyang wan <weiyangwan@gmail.com>
2016-08-11 16:50:48 +0800

    Rectify getting data from Wikipedia

--- scripts/script.js
+++ scripts/script.js
@@ -22,16 +22,31 @@ $(function() {
       $loader.hide();
       $( '<p class="outputtext">' + data.quote + '</p>' ).appendTo($quoteOutPut);
       $( '<p class="outputtext">' + ' - ' + data.author + '</p>' ).appendTo($quoteOutPut);
+      $.ajax({
+          type: "GET",
+          url: "http://en.wikipedia.org/w/api.php?callback=?",
+          // url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
+          //http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=Stack%20Overflow
+          contentType: "application/json;",
+          data: "prop=text&format=json&section=0&page=" + encodeURIComponent(data.author) + "&action=parse",
+          async: false,
+          dataType: "json",
+          success: function (data, textStatus, jqXHR) {
+            console.log(data);
+        $( '<p class="outputtext">An error occurred during your request: '  + data + ' ' + '</p>' ).appendTo($quoteOutPut);
+      },
+
       // $quoteOutPut.html(data.quote);
       // $quoteOutPut.html(data.author);
       //front end rendering
       // console.log("API Success");
-    }).fail(function(request, textStatus, errorThrown){
-      $loader.hide();
-      $( '<p class="outputtext">An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown + '</p>' ).appendTo($quoteOutPut);
-      // console.log("API Fail");
-      //front end rendering
-    });
-  });
-
+    })
+    // .fail(function(request, textStatus, errorThrown){
+    //   $loader.hide();
+    //   $( '<p class="outputtext">An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + '</p>' ).appendTo($quoteOutPut);
+    //   console.log(data);
+    //   console.log(data.author);
+    // }); //end of .fail
+  }); //end of .done
+});//end of submit
 });
