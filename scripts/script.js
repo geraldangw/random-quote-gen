48ac8be4c78d391550ffee2e9f3851b659fcc222
ben yang <benyang00@gmail.com>
2016-08-11 10:47:52 +0800

    added cloud backgound .png file

--- scripts/script.js
+++ scripts/script.js
@@ -8,6 +8,8 @@ $(function() {
   //click
   $submitbtn.on('click', function(e){
     e.preventDefault();
+    $(".outputtext").remove();
+    $(".outputtext").remove();
     //ajax
     $.ajax({
       url: 'https://andruxnet-random-famous-quotes.p.mashape.com/',
@@ -18,15 +20,15 @@ $(function() {
       },
     }).done(function(data){
       $loader.hide();
-      $( '<p>' + data.quote + '</p>' ).appendTo($quoteOutPut);
-      $( '<p>' + data.author + '</p>' ).appendTo($quoteOutPut);
+      $( '<p class="outputtext">' + data.quote + '</p>' ).appendTo($quoteOutPut);
+      $( '<p class="outputtext">' + ' - ' + data.author + '</p>' ).appendTo($quoteOutPut);
       // $quoteOutPut.html(data.quote);
       // $quoteOutPut.html(data.author);
       //front end rendering
       // console.log("API Success");
     }).fail(function(request, textStatus, errorThrown){
       $loader.hide();
-      $( '<p>An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown + '</p>' ).appendTo($quoteOutPut);
+      $( '<p class="outputtext">An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown + '</p>' ).appendTo($quoteOutPut);
       // console.log("API Fail");
       //front end rendering
     });
