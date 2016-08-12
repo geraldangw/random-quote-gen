0aed8740f8e81225b22ed59533f5d94a68dc6e0c
weiyang wan <weiyangwan@gmail.com>
2016-08-12 10:20:08 +0800

    Complete adding wiki search API

--- scripts/script.js
+++ scripts/script.js
@@ -27,26 +27,24 @@ $(function() {
           url: "http://en.wikipedia.org/w/api.php?callback=?",
           // url: "https://en.wikipedia.org/w/api.php?action=parse&format=json&prop=text&section=0&page=Jimi_Hendrix&callback=?",
           //http://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=Stack%20Overflow
+          // /w/api.php?action=query&format=json&prop=extracts&titles=Main+Page&exintro=1&explaintext=1
           contentType: "application/json;",
-          data: "prop=text&format=json&section=0&page=" + encodeURIComponent(data.author) + "&action=parse",
+          data: "action=opensearch&search=" + encodeURIComponent(data.author) + "&format=json",
+          // data: "prop=text&format=json&section=0&page=" + encodeURIComponent(data.author) + "&action=parse",
           async: false,
           dataType: "json",
           success: function (data, textStatus, jqXHR) {
-            console.log(data);
-        $( '<p class="outputtext">An error occurred during your request: '  + data + ' ' + '</p>' ).appendTo($quoteOutPut);
+            console.log(data[1]);
+            for (var i = 0; i < data[1].length; i++) {
+              $( '<a href="' + data[3][i] + '">' + '<p class="outputtext">Wiki Output Title: '  + data[1][i] + ' ' + '</p></a>' + '<p>' + data[2][i] + '</p>').appendTo($quoteOutPut);
+            }
       },
-
-      // $quoteOutPut.html(data.quote);
-      // $quoteOutPut.html(data.author);
-      //front end rendering
-      // console.log("API Success");
-    })
-    // .fail(function(request, textStatus, errorThrown){
-    //   $loader.hide();
-    //   $( '<p class="outputtext">An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + '</p>' ).appendTo($quoteOutPut);
-    //   console.log(data);
-    //   console.log(data.author);
-    // }); //end of .fail
+    }).fail(function(request, textStatus, errorThrown){
+      $loader.hide();
+      $( '<p class="outputtext">An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + '</p>' ).appendTo($quoteOutPut);
+      console.log(data);
+      console.log(data.author);
+    }); //end of .fail
   }); //end of .done
 });//end of submit
 });
